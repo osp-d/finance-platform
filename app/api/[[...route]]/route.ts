4538fbb412928adc66b94da05aa69624695986a9
osp-d <edudamirosp@gmail.com>
2024-09-16 11:36:03 +0500

    Add accounts GET API
    Connected RPC client. Added React Query. Added GET API for accounts.

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -2,13 +2,14 @@ import { Hono } from "hono";
 import { handle } from "hono/vercel";
 // import { z } from "zod";
 // import { zValidator } from "@hono/zod-validator";
-import test from "./test";
+import accounts from "@/app/api/[[...route]]/accounts";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
-
-app.route("/test", test);
+const routes = app.route("/accounts", accounts);
 
 export const GET = handle(app);
 export const POST = handle(app);
+
+export type AppType = typeof routes;
