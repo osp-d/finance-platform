6e78a4b54107024341641dc3d47966567e7ef65d
osp-d <edudamirosp@gmail.com>
2024-09-23 18:55:05 +0500

    Add transactions hooks

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,13 +4,15 @@ import { handle } from "hono/vercel";
 // import { zValidator } from "@hono/zod-validator";
 import accounts from "@/app/api/[[...route]]/accounts";
 import categories from "@/app/api/[[...route]]/categories";
+import transactions from "@/app/api/[[...route]]/transactions";
 
 export const runtime = "edge";
 
 const app = new Hono().basePath("/api");
 const routes = app
   .route("/accounts", accounts)
-  .route("/categories", categories);
+  .route("/categories", categories)
+  .route("/transactions", transactions);
 
 export const GET = handle(app);
 export const POST = handle(app);
