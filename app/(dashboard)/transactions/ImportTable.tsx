cb44be2d12db66f6a0f3018e839b7b1726d2c72c
osp-d <edudamirosp@gmail.com>
2024-09-29 12:58:25 +0500

    Add CSV transactions import page and functionality

---
+++ app/(dashboard)/transactions/ImportTable.tsx
@@ -0,0 +1,52 @@
+import {
+  Table,
+  TableHead,
+  TableHeader,
+  TableBody,
+  TableCell,
+  TableRow,
+} from "@/components/ui/table";
+import { TableHeadSelect } from "./TableHeadSelect";
+
+type Props = {
+  headers: string[];
+  body: string[][];
+  selectedColumns: Record<string, string | null>;
+  onTableHeadSelectChange: (columnIndex: number, value: string | null) => void;
+};
+
+export function ImportTable({
+  headers,
+  body,
+  selectedColumns,
+  onTableHeadSelectChange,
+}: Props) {
+  return (
+    <div className="overflow-hidden rounded-md border">
+      <Table>
+        <TableHeader className="bg-muted">
+          <TableRow>
+            {headers.map((_item, index) => (
+              <TableHead key={index}>
+                <TableHeadSelect
+                  columnIndex={index}
+                  selectedColumns={selectedColumns}
+                  onChange={onTableHeadSelectChange}
+                />
+              </TableHead>
+            ))}
+          </TableRow>
+        </TableHeader>
+        <TableBody>
+          {body.map((row: string[], index) => (
+            <TableRow key={index}>
+              {row.map((cell, index) => (
+                <TableCell key={index}>{cell}</TableCell>
+              ))}
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </div>
+  );
+}
--- app/(dashboard)/transactions/page.tsx
+++ app/(dashboard)/transactions/page.tsx
@@ -1,17 +1,46 @@
 "use client";
 
+import { useState } from "react";
 import { useNewTransaction } from "@/features/transactions/hooks/useNewTransaction";
 import { useGetTransactions } from "@/features/transactions/api/useGetTransactions";
 import { useBulkDeleteTransactions } from "@/features/transactions/api/useBulkDeleteTransactions";
 
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
-import { DataTable } from "@/app/(dashboard)/transactions/DataTable";
 import { columns } from "@/app/(dashboard)/transactions/columns";
 import { Loader2, Plus } from "lucide-react";
+import { DataTable } from "@/app/(dashboard)/transactions/DataTable";
 import { Skeleton } from "@/components/ui/skeleton";
+import { UploadButton } from "./UploadButton";
+import { ImportCard } from "./ImportCard";
+
+enum VARIANTS {
+  LIST = "LIST",
+  IMPORT = "IMPORT",
+}
+
+const INITIAL_IMPORT_RESULTS = {
+  data: [],
+  errors: [],
+  meta: {},
+};
 
 export default function TransactionsPage() {
+  const [variant, setVariant] = useState<VARIANTS>(VARIANTS.LIST);
+  const [importResults, setImportResults] = useState(INITIAL_IMPORT_RESULTS);
+
+  const onUpload = (results: typeof INITIAL_IMPORT_RESULTS) => {
+    console.log(results);
+
+    setImportResults(results);
+    setVariant(VARIANTS.IMPORT);
+  };
+
+  const onCancelImport = () => {
+    setImportResults(INITIAL_IMPORT_RESULTS);
+    setVariant(VARIANTS.LIST);
+  };
+
   const newTransaction = useNewTransaction();
   const transactionsQuery = useGetTransactions();
   const transaction = transactionsQuery.data || [];
@@ -35,6 +64,18 @@ export default function TransactionsPage() {
     );
   }
 
+  if (variant === VARIANTS.IMPORT) {
+    return (
+      <>
+        <ImportCard
+          data={importResults.data}
+          onCancel={onCancelImport}
+          onSubmit={() => {}}
+        />
+      </>
+    );
+  }
+
   return (
     <div className="mx-auto -mt-24 w-full max-w-screen-2xl pb-10">
       <Card className="border-none drop-shadow-sm">
@@ -42,10 +83,17 @@ export default function TransactionsPage() {
           <CardTitle className="line-clamp-1 text-xl">
             Transaction History
           </CardTitle>
-          <Button size="sm" onClick={newTransaction.onOpen}>
-            <Plus className="mr-2 size-4" />
-            Add new
-          </Button>
+          <div className="flex flex-col items-center gap-x-2 gap-y-2 lg:flex-row">
+            <Button
+              size="sm"
+              onClick={newTransaction.onOpen}
+              className="w-full lg:w-auto"
+            >
+              <Plus className="mr-2 size-4" />
+              Add new
+            </Button>
+            <UploadButton onUpload={onUpload} />
+          </div>
         </CardHeader>
 
         <CardContent>
